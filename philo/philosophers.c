8ab31947f94157c00f0250c4cec7e23721123c6c
Rosie <margaryanrosie@gmail.com>
2025-10-15 14:56:54 +0400

    data races in main fixed

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -48,15 +48,19 @@ int	main(int argc, char *argv[])
 		return (1);
 	}
 	i = 0;
+	pthread_mutex_lock(&shared.death_mutex);
 	shared.someone_died = 0;
+	pthread_mutex_unlock(&shared.death_mutex);
 	shared.start_time = get_time_in_milliseconds();         //we set the start time when the simulation starts
 	//after this we need to initialize the last_meal_time of each philosopher with this start time
 	//(*thats why we dont initilize it in the init_philosophers)
 	while (i < shared.number_of_philosophers)
 	{
+		pthread_mutex_lock(&shared.last_meal_mutex);
 		shared.philosophers[i].last_meal_time = shared.start_time;
 		shared.philosophers[i].meals_eaten = 0;
 		i++;
+		pthread_mutex_unlock(&shared.last_meal_mutex);
 	}
 	i = 0;
 	while (i < shared.number_of_philosophers)
